7567f515613b2d0b8c1d59b29f6fe1f043aaf303
Programmer707 <replydeveshkumar@gmail.com>
2020-07-26 16:13:02 -0400

    added more code

---
+++ Quotes App/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken.'),
+    Quote(author: 'Thomas Jefferson', text: 'Nothing gives one person so much advantage over another as to remain always cool and unruffled under all circumstances.'),
+    Quote(author: 'Pablo Picasso', text: 'Learn the rules like a pro, so that you can break them like an artist.'),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            }
+
+        )).toList(),
+      ),
+    );
+  }
+}
+
